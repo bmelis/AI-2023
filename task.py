67679a7bed6cd36ad560672a5f15ef03bcfdedcd
Bent Melis <r0831245@student.thomasmore.be>
2023-10-03 23:31:42 +0200

    streamlit test

--- task.py
+++ task.py
@@ -1,55 +1,42 @@
 import streamlit as st
 from simpleai.search import CspProblem, backtrack
 
-header = st.container()
-
-with header:
-    st.title("Taak 1 AI - Bent Melis (r0831245)")
-
-input_first = st.text_input("Enter the first word: ").upper()
-input_sec = st.text_input("Enter the second word: ").upper()
-input_result = st.text_input("Enter the result word: ").upper()
+input_first = input("Enter the first word: ").upper()
+input_sec = input("Enter the second word: ").upper()
+input_result = input("Enter the result word: ").upper()
 
 variables = tuple(input_first) + tuple(input_sec) + tuple(input_result)
-print(input_first)
-print(input_sec)
-print("+------------------")
-print(input_result)
+st.text(input_first)
+st.text(input_sec)
+st.text("+------------------")
+st.text(input_result)
 
 variables = set(variables)
 
-# Define the list of values that each variable can take
 domains = {vari: list(range(1, 10)) if vari in (input_first[0], input_sec[0], input_result[0]) else list(range(0, 10)) for vari in variables}
 
 def constraint_unique(variable, value):
-    # Ensure that each variable has a unique value
     return len(value) == len(set(value))
 
 def constraint_add(variables, values):
-    # Extract the input words and the result word
     input_words = [input_first, input_sec]
     result_word = input_result
 
-    # Initialize dictionaries to store the values of each word
     word_values = [{letter: None for letter in word} for word in input_words + [result_word]]
 
-    # Assign values to the letters based on the variable assignments
     for letter, value in zip(variables, values):
         for i, word in enumerate(word_values):
             if letter in word:
                 word[letter] = value
 
-    # Calculate the numeric representation of each word
     input_numeric = [int("".join([str(word_values[i][letter]) for letter in word])) for i, word in enumerate(input_words)]
     result_numeric = int("".join([str(word_values[2][letter]) for letter in result_word]))
 
-    # Check if the addition of input words matches the result word
     return sum(input_numeric) == result_numeric
-
+    
 constraints = [
-    (vari, constraint_unique) for vari in variables  # Ensure unique values for each variable
-] + [
-    (variables, constraint_add)  # Ensure the addition constraint
+    (variables, constraint_unique),
+    (variables, constraint_add)
 ]
 
 problem = CspProblem(variables, domains, constraints)
@@ -57,14 +44,13 @@ problem = CspProblem(variables, domains, constraints)
 output = backtrack(problem)
 
 if output is not None:
-    # Extract the values assigned to the variables
     values = {vari: output[vari] for vari in variables}
 
-    # Create the formatted strings for the input words and the horizontal line
     input_first_str = input_first
     input_sec_str = input_sec
     result_str = input_result
     line = "+"
+
 
     for vari in variables:
         if vari in input_first:
@@ -76,9 +62,10 @@ if output is not None:
             line += "-" * len(str(values[vari]))
         else:
             line += " "
+            
+    st.text(input_first_str)
+    st.text(input_sec_str)
+st.text("+------------------")    
+st.text(result_str)
 
-    # Print the formatted result
-    print(input_first_str)
-    print(input_sec_str)
-    print("+------------------")    
-    print(result_str)
+#print('\nSolutions:', output)
